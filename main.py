32df494d748d371dd82b95f861deebeba72ad56e
Jason Wei <jasonwei99@gmail.com>
2020-09-04 00:57:00 +0000

    Added .0

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ for i in range(3):
   credit = input(f"Enter your course {i+1} credit: ")
 
   if grade == "A":
-    point = 4
+    point = 4.0
   elif grade == "A-":
     point = 3.67
   elif grade == "B":
-    point = 3
+    point = 3.0
   elif grade == "B+":
     point = 3.33
   elif grade == "B-":
@@ -22,13 +22,13 @@ for i in range(3):
   elif grade =="C+":
     point = 2.33
   elif grade == "C":
-    point = 2
+    point = 2.0
   elif grade == "D":
-    point = 1
+    point = 1.0
   else:
-    point = 0
+    point = 0.0
 
-  add = point*int(credit)   #CALCULATES NUMERATOR
+  add = float(point)*int(credit)   #CALCULATES NUMERATOR
   a.append(add)             #ADDS PROCESSED NUMBER TO LIST
   cre.append(int(credit))   #ADDS CREDITS TO LIST
 
